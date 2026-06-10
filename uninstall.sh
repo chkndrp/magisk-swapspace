9038fea150611752c1fd39f7349b51e98f2729d4
chickendrop89 <chickendrop89@gmail.com>
2026-06-10 22:30:19 +0200

    mss: uninstall.sh: lint this file to mute shellcheck
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,24 @@
 # Don't modify anything after this
-if [ -f "$INFO" ]; then
-  while read LINE; do
-    if [ "$(echo -n "$LINE" | tail -c 1)" == "~" ]; then
-      continue
-    elif [ -f "$LINE~" ]; then
-      mv -f "$LINE"~ "$LINE"
-    else
-      rm -f "$LINE"
-      while true; do
-        LINE=$(dirname "$LINE")
-        [ "$(ls -A "$LINE" 2>/dev/null)" ] && break 1 || rm -rf "$LINE"
-      done
-    fi
-  done < "$INFO"
-  rm -f "$INFO"
+if [ -f "$INFO" ]; 
+  then
+    while IFS= read -r line; 
+      do
+        if [ "${line#"${line%?}"}" = "~" ]; 
+          then continue
+        elif [ -f "$line~" ]; 
+          then mv -f "$line~" "$line"
+        else
+          dir="$line"
+          rm -f "$line"
+          while [ "$dir" != "." ] && [ "$dir" != "/" ]; 
+            do
+              dir=$(dirname "$dir")
+              if [ -n "$(ls -A "$dir" 2>/dev/null)" ]; 
+                then break
+              fi
+              rmdir "$dir" 2>/dev/null || break
+          done
+        fi
+    done < "$INFO"
+    rm -f "$INFO"
 fi
